bbaeed30d7e76bd4b0ce488b798d2debeb29e9b8
ViktorMun <v.mun@yahoo.com>
2018-03-26 14:48:25 +0200

    Finish entities.js

--- src/entities.ts
+++ src/entities.ts
@@ -1,18 +1,59 @@
-import { BaseEntity, PrimaryGeneratedColumn, Column, Entity, Index, OneToMany, ManyToOne } from 'typeorm'
+import { Entity, PrimaryGeneratedColumn, Column, OneToMany, ManyToOne } from 'typeorm'
+import { BaseEntity } from 'typeorm/repository/BaseEntity'
+import { IsString, IsNumber, IsBoolean } from 'class-validator'
 
 @Entity()
-export class Answers extends BaseEntity {
+export class Quiz extends BaseEntity {
+
+  @PrimaryGeneratedColumn()
+  id?: number;
+
+  @IsString()
+  @Column('text', { nullable: false })
+  title: string;
+
+  @OneToMany(_ => Question, question => question.quiz, {eager:true})
+  question: Question[]
+
+}
+
+@Entity()
+export class Question extends BaseEntity {
 
   @PrimaryGeneratedColumn()
   id?: number
 
-  @Column('number', { nullable: true })
-  questionId: number
+  @IsString()
+  @Column('text', {nullable:false})
+  text: string
 
+  @IsString()
+  @Column('text', {nullable:false})
+  type: string
+
+  @ManyToOne(_ => Quiz, quiz => quiz.question)
+  quiz: Quiz
+
+  @OneToMany(_ => Answer, answer => answer.question, {eager:true})
+  answer: Answer[]
+
+}
+
+@Entity()
+export class Answer extends BaseEntity {
+
+  @PrimaryGeneratedColumn()
+  id?: number
+
+  @IsBoolean()
   @Column('boolean', {nullable: true})
   correct: boolean
 
+  @IsString()
   @Column('text', { nullable: true})
   text: string
+
+  @ManyToOne(_ => Question, question => question.answer)
+  question: Question
 
 }
